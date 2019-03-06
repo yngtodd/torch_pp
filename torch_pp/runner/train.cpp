32d0e3478fbe170982281aee0435ac7e88a884f9
yngtodd <young.todd.mk@gmail.com>
2019-03-06 01:36:16 -0500

    clean up

--- torch_pp/runner/train.cpp
+++ torch_pp/runner/train.cpp
@@ -8,9 +8,11 @@ int main() {
     //auto net = std::make_shared<mlp::MLP>();
     mlp::MLP net;
 
-    for (const auto& pair : net.named_parameters()) {
-        std::cout << pair.key() << ": " << pair.value() << std::endl;
-    }
+    /*
+     * for (const auto& pair : net.named_parameters()) {
+     *   std::cout << pair.key() << ": " << pair.value() << std::endl;
+     * }
+     */
 
     std::cout << net.forward(torch::ones({2, 784})) << std::endl;
 }
