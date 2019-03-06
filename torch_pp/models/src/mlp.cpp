72476f18782e8ea16325dedaf8b24c0c94c9c319
yngtodd <young.todd.mk@gmail.com>
2019-03-06 02:28:16 -0500

    braces

--- torch_pp/models/src/mlp.cpp
+++ torch_pp/models/src/mlp.cpp
@@ -4,7 +4,8 @@ namespace mlp {
 
     void print_parameters(mlp::MLP net)
     {
-        for (const auto& pair : net.named_parameters()) {
+        for (const auto& pair : net.named_parameters()) 
+        {
             std::cout << pair.key() << ": " << pair.value() << std::endl;
         }
     }
