bac3fb05aa4810039d8c2329c128cf2c77966f58
yngtodd <young.todd.mk@gmail.com>
2019-03-06 01:33:50 -0500

    closing out

--- torch_pp/runner/train.cpp
+++ torch_pp/runner/train.cpp
@@ -5,4 +5,12 @@
 
 int main() {
     std::cout << "Hello World!" << std::endl;
+    //auto net = std::make_shared<mlp::MLP>();
+    mlp::MLP net;
+
+    for (const auto& pair : net.named_parameters()) {
+        std::cout << pair.key() << ": " << pair.value() << std::endl;
+    }
+
+    std::cout << net.forward(torch::ones({2, 784})) << std::endl;
 }
